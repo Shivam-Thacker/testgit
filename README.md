ab2395050334c86bf105fb33ca0f94266dd00402
Shivam-Thacker <shivamthacker05@gmail.com>
2020-01-02 14:42:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# wotnot package
+
+This is an wotnot package allows you to fetch data from github repository 
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='wotnot',
+    packages=setuptools.find_packages(),
+    description='Hello world enterprise edition',
+    version='0.0.1',
+    url='https://github.com/Shivam-Thacker/wotnot',
+    author='Shivam',
+    author_email='shivamthacer05@gmail.com',
+    keywords=['pip','wotnot']
+)
+
